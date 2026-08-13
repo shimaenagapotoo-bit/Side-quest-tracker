2b3861d3b9d9cde82ab21c606031082130124b6a
shimaenagapotoo-bit <shimaenaga.potoo@gmail.com>
2026-08-13 12:28:09 +0000

    Add quest input form

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,21 @@ import streamlit as st
 
 st.title("AY SIDE QUEST") 
 st.write("EXPLORE WHO I CAN BECOME.")
+
+st.divider()
+
+st.subheader("NEW QUEST")
+
+quest = st.text_input("What is your next quest?",
+                      placeholder = "例:英会話レッスンに参加する"
+                      )
+category = st.selectbox(
+    "Category",
+    ["Adventure","Learning","Fitness","Social","Creative"]
+)
+if st.button("START QUEST"):
+    if quest : 
+        st.success(f"QUEST ADDED:{quest}")
+        st.write(f"Category:{category}")
+    else:
+        st.warning("クエストを入力してください")    
